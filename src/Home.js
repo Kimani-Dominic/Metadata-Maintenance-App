d0e201d7e48c1e3aa4e37736583cc55b7c42f938
Kimani-Dominic <muiruridominic001@gmail.com>
2024-08-27 01:35:08 +0300

    Refactor navigation to use onClick handlers instead of react-router-dom

--- src/Home.js
+++ src/Home.js
@@ -24,14 +24,16 @@ import {
     Menu,
     IconDuplicate24,
     IconShare24,
+    IconAdd24,
     // Select,   
     // Option
 } from '@dhis2/ui';
 // import { Link, Navigate, useNavigate } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import './components/sidebar.css';
+import EditElement from './components/EditElement';
 
-let pageSize = 20;
+let pageSize = 10;
 
 const myQuery = {
     results: {
@@ -133,176 +135,186 @@ const Home = () => {
     // };
 
     return (
-        <div className='main-content'>
-            <Sidebar />
-            <div className='search'>
-            <div className='searching'>
-             <InputField
-                value={dataElementSearch}
-                onChange={handleInputChange(setDataElementSearch)}
-                // onChange={(e) => setDataElementSearch(e.target.value)}
-                type="search"
-                placeholder="Search data elements"
-            />
-            <InputField
-                value={domainTypeSearch}
-                onChange={(e) => setDomainTypeSearch(e.target.value)}
-                type="search"
-                placeholder="Domain Type"
-            />
-            <InputField
-                value={valueTypeSearch}
-                onChange={(e) => setValueTypeSearch(e.target.value)}
-                type="search"
-                placeholder="Value Type"
-            />
-            <InputField
-                value={dataSetSearch}
-                onChange={(e) => setDataElementSearch(e.target.value)}
-                type="search"
-                placeholder="Data Set"
-            />
-            <InputField
-                value={categoryComboSearch}
-                onChange={(e) => setCategoryComboSearch(e.target.value)}
-                type="search"
-                placeholder="Category combo"
-            />
+        <><Sidebar />
+        <div>
+            <div className='header'>
+            <div className='breadcrumbs'>
+            <h2 className='header'>Data Element Management</h2>
+            <div className='filter-section'>
+                
+                    <InputField
+                        value={dataElementSearch}
+                        onChange={handleInputChange(setDataElementSearch)}
+                        // onChange={(e) => setDataElementSearch(e.target.value)}
+                        type="search"
+                        placeholder="Search data elements" />
+                    <InputField
+                        value={domainTypeSearch}
+                        onChange={(e) => setDomainTypeSearch(e.target.value)}
+                        type="search"
+                        placeholder="Domain Type" />
+                    <InputField
+                        value={valueTypeSearch}
+                        onChange={(e) => setValueTypeSearch(e.target.value)}
+                        type="search"
+                        placeholder="Value Type" />
+                    <InputField
+                        value={dataSetSearch}
+                        onChange={(e) => setDataElementSearch(e.target.value)}
+                        type="search"
+                        placeholder="Data Set" />
+                    <InputField
+                        value={categoryComboSearch}
+                        onChange={(e) => setCategoryComboSearch(e.target.value)}
+                        type="search"
+                        placeholder="Category combo" />
+
+                         <Button>Clear all filters</Button>
+                
             </div>
+            
+
+         </div>
             </div>
-            <NewElement refetch={refetch} />
+            <div className="action-buttons">
+           <Button icon={<IconAdd24 />}> New</Button>
+            <Button>Download</Button>
+            <Button>Manage View</Button>
+            </div>
+
+
             <div className='table-container'>
-            <DataTable>
-                <TableHead width="48px">
-                    <DataTableRow>
-                        <DataTableColumnHeader>
-                            <Checkbox
-                                checked={selected.length === data.results.dataElements.length}
-                                onChange={toggleAll}
-                            />
-                        </DataTableColumnHeader>
-                        <DataTableColumnHeader
-                            name="displayName"
-                            onSortIconClick={() => onSortIconClick('displayName')}
-                            sortDirection={sortField === 'displayName' ? sortDirection : 'default'}
-                            sortIconTitle="Sort by displayName"
-                        >
-                            Name
-                        </DataTableColumnHeader>
-                        <DataTableColumnHeader
-                            name="domainType"
-                            onSortIconClick={() => onSortIconClick('domainType')}
-                            sortDirection={sortField === 'domainType' ? sortDirection : 'default'}
-                            sortIconTitle="Sort by domainType"
-                        >
-                            Domain Type
-                        </DataTableColumnHeader>
-                        <DataTableColumnHeader
-                            name="valueType"
-                            onSortIconClick={() => onSortIconClick('valueType')}
-                            sortDirection={sortField === 'valueType' ? sortDirection : 'default'}
-                            sortIconTitle="Sort by valueType"
-                        >
-                            Value Type
-                        </DataTableColumnHeader>
-                        <DataTableColumnHeader
-                            name="categoryCombo"
-                            onSortIconClick={() => onSortIconClick('categoryCombo')}
-                            sortDirection={sortField === 'categoryCombo' ? sortDirection : 'default'}
-                            sortIconTitle="Sort by categoryCombo"
-                        >
-                            Category Combo
-                        </DataTableColumnHeader>
-                        <DataTableColumnHeader>
-                            <IconSettings24 />
-                        </DataTableColumnHeader>
-                    </DataTableRow>
-                </TableHead>
-                <TableBody>
-                    {data.results.dataElements.map((dataElement) => (
-                        <DataTableRow key={dataElement.id}>
-                            <DataTableCell>
+                <DataTable>
+                    <TableHead width="48px">
+                        <DataTableRow>
+                            <DataTableColumnHeader>
                                 <Checkbox
-                                    checked={selected.includes(dataElement.id)}
-                                    onChange={() => toggleSelected(dataElement.id)}
-                                    value={dataElement.id}
-                                />
-                            </DataTableCell>
-                            <DataTableCell>{dataElement.displayName}</DataTableCell>
-                            <DataTableCell>{dataElement.domainType}</DataTableCell>
-                            <DataTableCell>{dataElement.valueType}</DataTableCell>
-                            <DataTableCell>{dataElement.categoryCombo.name}</DataTableCell>
-                            <DataTableCell>{dataElement.lastUpdated}</DataTableCell>
-                            <DataTableCell>
-                                {/* <Link to={`/edit/${dataElement.id}`}>
-                                    <IconEdit24 />
-                                </Link> */}
+                                    checked={selected.length === data.results.dataElements.length}
+                                    onChange={toggleAll} />
+                            </DataTableColumnHeader>
+                            <DataTableColumnHeader
+                                name="displayName"
+                                onSortIconClick={() => onSortIconClick('displayName')}
+                                sortDirection={sortField === 'displayName' ? sortDirection : 'default'}
+                                sortIconTitle="Sort by displayName"
+                            >
+                                Name
+                            </DataTableColumnHeader>
+                            <DataTableColumnHeader
+                                name="domainType"
+                                onSortIconClick={() => onSortIconClick('domainType')}
+                                sortDirection={sortField === 'domainType' ? sortDirection : 'default'}
+                                sortIconTitle="Sort by domainType"
+                            >
+                                Domain Type
+                            </DataTableColumnHeader>
+                            <DataTableColumnHeader
+                                name="valueType"
+                                onSortIconClick={() => onSortIconClick('valueType')}
+                                sortDirection={sortField === 'valueType' ? sortDirection : 'default'}
+                                sortIconTitle="Sort by valueType"
+                            >
+                                Value Type
+                            </DataTableColumnHeader>
+                            <DataTableColumnHeader
+                                name="categoryCombo"
+                                onSortIconClick={() => onSortIconClick('categoryCombo')}
+                                sortDirection={sortField === 'categoryCombo' ? sortDirection : 'default'}
+                                sortIconTitle="Sort by categoryCombo"
+                            >
+                                Category Combo
+                            </DataTableColumnHeader>
+                            <DataTableColumnHeader
+                                name="Last Updated">
+                                Last Updated
+                            </DataTableColumnHeader>
+                            <DataTableColumnHeader>
+                                <IconSettings24 />
+                            </DataTableColumnHeader>
+                        </DataTableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.results.dataElements.map((dataElement) => (
+                            <DataTableRow key={dataElement.id}>
+                                <DataTableCell>
+                                    <Checkbox
+                                        checked={selected.includes(dataElement.id)}
+                                        onChange={() => toggleSelected(dataElement.id)}
+                                        value={dataElement.id} />
+                                </DataTableCell>
+                                <DataTableCell>{dataElement.displayName}</DataTableCell>
+                                <DataTableCell>{dataElement.domainType}</DataTableCell>
+                                <DataTableCell>{dataElement.valueType}</DataTableCell>
+                                <DataTableCell>{dataElement.categoryCombo.name}</DataTableCell>
+                                <DataTableCell>{dataElement.lastUpdated}</DataTableCell>
+                                <DataTableCell>
+                                <div style={{ display: 'flex', gap: '2px' }}>
+                                    <Button icon={<IconEdit24 />} />
+                                    <Button icon={<IconMore24 />} />
+                                </div>
+                                                                    {/* <Link to={`/edit/${dataElement.id}`}>
+                <IconEdit24 />
+            </Link> */}
                                     {/* <IconMore24 /> */}
-                                     {/* <DeleteElement id={dataElement.id} refetch={refetch} />  */}
+                                    {/* <DeleteElement id={dataElement.id} refetch={refetch} />  */}
                                     {/* <div>
-                                        <Button onClick={handleDelete} disabled={loading}>
-                                            <IconMore24 />
-                                        </Button>
-                                        {error && (
-                                            <AlertBar permanent critical>
-                                                Error: {error.message}
-                                            </AlertBar>
+                <Button onClick={handleDelete} disabled={loading}>
+                    <IconMore24 />
+                </Button>
+                {error && (
+                    <AlertBar permanent critical>
+                        Error: {error.message}
+                    </AlertBar>
+                )}
+            </div> */}
+
+
+                                    {/* <div>
+                                        <Button icon={<IconMore24 />} onClick={toggleMenu} />
+                                        {isMenuOpen && (
+                                            <Popper placement="bottom-start" onClickOutside={() => setIsMenuOpen(false)}>
+                                                <Menu>
+                                                    <MenuItem
+                                                        icon={<IconEdit24 />}
+                                                        label="Edit"
+                                                        onClick={() => navigate(`/edit/${dataElement.id}`)} />
+                                                    <MenuItem
+                                                        icon={<IconDuplicate24 />}
+                                                        label="Clone"
+                                                        onClick={() => alert('Do you want to copy the Element API path?')} />
+
+                                                    <MenuItem
+                                                        icon={<IconShare24 />}
+                                                        label="Sharing Settings"
+                                                        onClick={() => alert('Sharing action triggered')} />
+                                                    <MenuItem
+                                                        icon={<DeleteElement dataElementId={dataElement.id} onDelete={onChange} />} />
+                                                    <MenuItem
+                                                        // icon={<IconDetails24 />}
+                                                        label="Show details"
+                                                        onClick={() => alert('Show Data Element details')} />
+                                                </Menu>
+
+
+                                            </Popper>
                                         )}
                                     </div> */}
 
-                                    <div>
-                                    <Button icon={<IconMore24 />} onClick={toggleMenu} />
-                                    {isMenuOpen && (
-                                        <Popper placement="bottom-start" onClickOutside={() => setIsMenuOpen(false)}>
-                                            <Menu>
-                                                <MenuItem
-                                                        icon={<IconEdit24 />}
-                                                        label="Edit"
-                                                        onClick={() => navigate(`/edit/${dataElement.id}`)}
-                                                        
 
-                                                    />
-                                                <MenuItem
-                                                    icon={<IconDuplicate24 />}
-                                                    label="Clone"
-                                                    onClick={() => alert('Do you want to copy the Element API path?')}
-                                                />
-                                                
-                                                <MenuItem
-                                                    icon={<IconShare24 />}
-                                                    label="Sharing Settings"
-                                                    onClick={() => alert('Sharing action triggered')}
-                                                />
-                                                <MenuItem
-                                                    icon={<DeleteElement dataElementId={dataElement.id} onDelete={onChange} />}
-                                                />
-                                                <MenuItem
-                                                    // icon={<IconDetails24 />}
-                                                    label="Show details"
-                                                    onClick={() => alert('Show Data Element details')}
-                                                />
-                                            </Menu>
-
-
-                                        </Popper>
-                                    )}
-                                            </div>
-                                                                
-                        
-                            </DataTableCell>
-                        </DataTableRow>
-                    ))}
-                </TableBody>
-            </DataTable>
+                                </DataTableCell>
+                            </DataTableRow>
+                        ))}
+                    </TableBody>
+                </DataTable>
+            </div>
             </div>
             <Pagination
                 page={page}
                 pageCount={Math.ceil(data.results.pager.total / pageSize)}
                 pageSize={pageSize}
                 onPageChange={handlePageChange}
-                className="pagination"
-            />
-        </div>
+                className="pagination" />
+        </>
     );
 };
 
