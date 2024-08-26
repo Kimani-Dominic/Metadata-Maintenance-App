be01d004fc32b0cf82548cf76b2b15ecabbad4e9
Kimani-Dominic <muiruridominic001@gmail.com>
2024-08-26 22:33:53 +0300

    Refactor navigation to use onClick handlers instead of react-router-dom

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,33 +1,12 @@
-// import React from 'react';
-// import { NavLink } from 'react-router-dom';
-// import './sidebar.css';
-
-// const Sidebar = () => {
-//     return(
-//         <nav className="sidebar">
-//             <ul>
-//                 <li><NavLink to="/overview" activeClassName="active">Metadata Overview</NavLink></li>
-//                 <li><NavLink to="/" activeClassName="active">Categories</NavLink></li>
-//                 <li><NavLink to="/data-elements" activeClassName="active">Data elements</NavLink></li>
-//                 <li><NavLink to="/data-elements/overview" activeClassName="active">Overview</NavLink></li> 
-//                 <li><NavLink to="/data-elements/groups" activeClassName="active">Data element group</NavLink></li>
-//                 <li><NavLink to="/data-elements/group-sets" activeClassName="active">Data element group set</NavLink></li>
-//                 <li><NavLink to="/" activeClassName="active">Data sets</NavLink></li>
-//                 <li><NavLink to="/" activeClassName="active"></NavLink></li>
-//                 <li><NavLink to="/" activeClassName="active">Organisation units</NavLink></li>
-//                 <li><NavLink to="/" activeClassName="active">Programs and Tracker</NavLink></li>
-//                 <li><NavLink to="/" activeClassName="active">Validations</NavLink></li>
-//             </ul>
-//         </nav>
-// );
-// }
-
-// export default Sidebar;
-
-import React from 'react';
+import React, {useState} from 'react';
 import './sidebar.css';
 
-const Sidebar = ({ onMenuItemClick }) => {
+const Sidebar = () => {
+    const [selectedMenuItem, setSelectedMenuItem] = useState('/');
+    const onMenuItemClick = (menuItem) => {
+        setSelectedMenuItem(menuItem);
+    };
+
     return (
         <div className='sidebar'>
             <ul>
