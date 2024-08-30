4623260ee3a8604acc2a40fe8ee6d164e254855b
Kimani-Dominic <muiruridominic001@gmail.com>
2024-08-30 23:58:48 +0300

    Refactor navigation to use onClick handlers instead of react-router-dom

--- src/components/DeleteElement.js
+++ src/components/DeleteElement.js
@@ -1,18 +1,21 @@
 import { useDataMutation } from '@dhis2/app-runtime'
-import { Button, IconDelete16 } from '@dhis2/ui'
+// import { Button, IconDelete16, MenuItem } from '@dhis2/ui'
 
 const mutation = {
     resource: 'dataElements',
-    type: 'delete',
-    id: ({ id }) => id
+    id: ({ id }) => id,
+    type: 'delete'
 }
 
-const DeleteElement = ({ id, refetch }) => {
-    const [mutate, { loading }] = useDataMutation(mutation)
-    const onClick = () => {
-        mutate({ id }).then(refetch)
-    }
-    return <Button icon={<IconDelete16 />} destructive disabled={loading} onClick={onClick}>Delete</Button>
+const DeleteElement = ({ dataElementId, onDelete, triggerDelete }) => {
+    const [Delete] = useDataMutation(mutation, {
+        onComplete: onDelete,
+        variables: {
+            id: dataElementId,
+        },
+    })
+
+    return triggerDelete ? triggerDelete(Delete) : null;
 }
 
-export default  DeleteElement;
+export default DeleteElement;
